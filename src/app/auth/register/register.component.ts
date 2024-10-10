700bf97a5faf2128f0ef24f9d3435b83f0d26614
Gabriel DP <gabrieldominguezprado@gmail.com>
2024-10-10 12:57:11 -0300

    Estructura, HTML y estilos de la aplicación

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  public focusEmail: boolean = false;
+  public focusPassword: boolean = false;
+}
