9bddade36ece43e2043794d5c2f110f43a025b54
Gabriel DP <gabrieldominguezprado@gmail.com>
2024-10-10 14:54:09 -0300

    Sitema de ruteo + rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+// Módulos
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+// Rutas
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', 
+    component: DashboardComponent ,
+    children: dashboardRoutes
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
